7df9d3d3296fe0078d3d0673a186f01c8d53fb6f
kunalsb24 <kunalbhajbhuje5@gmail.com>
2026-01-21 23:15:00 +0530

    2 Problems on searching

--- Searching/tripletinSorted.cpp
+++ Searching/tripletinSorted.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
 
-bool isPair(int arr[], int n, int sum){
-    int i = 0, j = n-1;
+bool isPair(int arr[], int n, int sum, int si){
+    int i = si, j = n-1;
     while(i<j){
         if(arr[i]+arr[j] == sum)
             return true;
@@ -15,16 +15,49 @@ bool isPair(int arr[], int n, int sum){
 }
 
 bool isTriplet(int arr[], int n, int sum){
-    for(int i=0; i<n; i++)
+    for(int i=0; i < n-2; i++)
         for(int j=i+1; j<n; j++)
-            if(isPair(arr,n,sum - arr[i]))
-                return true;
-    
+            if(isPair(arr,n,sum - arr[i],i+1))
+                return true;    
     return false;
 }
 
+// Prints all the possible triplets
+
+void printTriplets(int arr[], int n, int sum){
+    for(int i = 0; i < n - 2; i++){
+        int l = i + 1;
+        int r = n - 1;
+
+        while(l < r){
+            int s = arr[i] + arr[l] + arr[r];
+
+            if(s == sum){
+                cout << "(" << arr[i] << ", "
+                            << arr[l] << ", "
+                            << arr[r] << ")" << endl;
+                l++;
+                r--;
+            }
+            else if(s < sum){
+                l++;
+            }
+            else{
+                r--;
+            }
+        }
+    }
+}
+
+
 int main(){
     int n = 7, arr[] = {2,3,4,8,9,20,40}, sum = 32;
-    cout << isTriplet(arr,n,sum);
+
+    if(isTriplet(arr,n, sum))
+        cout << "True" << endl;
+    else
+        cout << "False" << endl;
+    
+    printTriplets(arr,n,sum);
     return 0;
 }
