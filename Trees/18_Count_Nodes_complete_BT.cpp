7f7ce8fbfbef8e08505b0c14d06e5290c11e4b04
kunalsb24 <kunalbhajbhuje5@gmail.com>
2026-07-10 15:03:16 +0530

    4 Trees Problems

---
+++ Trees/18_Count_Nodes_complete_BT.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int key;
+    Node*left;
+    Node*right;
+    Node(int k){
+        key=k;
+        left=right=NULL;
+    }
+};
+
+// Naive: O(n)
+// int countNode(Node*root){
+//     if(root==NULL) return 0;
+//     return 1 + countNode(root->left) + countNode(root->right);
+// }
+
+//---------------------------------------------------------------------
+
+// Efficient: O(logn * logn)
+
+int countNode(Node*root){
+    int lh=0, rh=0;
+    Node*curr=root;
+    while(curr!=NULL){
+        lh++;
+        curr = curr -> left;
+    }
+
+    curr = root;
+    while(curr != NULL){
+        rh++;
+        curr = curr->right;
+    }
+
+    if(lh == rh)
+        return pow(2,lh) - 1;
+    
+    return 1 + countNode(root ->left) + countNode(root -> right);
+}
+
+int main() {
+	
+	Node *root=new Node(10);
+	root->left=new Node(20);
+	root->right=new Node(30);
+	root->right->left=new Node(40);
+	root->right->right=new Node(50);
+	
+	cout<<countNode(root);
+}
---
+++ Trees/20_Inorder_Iterative.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int key;
+    Node*left;
+    Node*right;
+    Node(int k){
+        key=k;
+        left=right=NULL;
+    }
+};
+
+void iterativeInorder(Node*root){
+    stack<Node*> s;
+    Node*curr = root;
+    while(curr!=NULL || s.empty()==false){
+        while(curr!=NULL){
+            s.push(curr);
+            curr=curr->left;
+        }
+        curr = s.top();
+        s.pop();
+        cout << curr->key << " ";
+        curr = curr->right;
+    }
+}
+
+int main(){
+    Node *root = new Node(10);
+    root -> left = new Node(20);
+    root -> right = new Node(30);
+    root -> left -> left = new Node(40);
+    root -> left -> right = new Node(50);
+    root -> right -> right = new Node(70);
+    root -> right -> right -> right = new Node(80);
+
+    iterativeInorder(root);
+
+    return 0;
+
+}
